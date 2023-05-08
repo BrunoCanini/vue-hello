b4efa432a438f5c7fd27ccee64ba110e71154531
Bruno Canini <brunocanini1994@gmail.com>
2023-05-08 16:29:02 +0200

    add img

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ createApp({
     data() {
       return {
         message: 'Hello Vue, I hope we will be friends',
-        mieClassi: "colorWhite textCenter"
+        mieClassi: "colorWhite textCenter",
+        srcImg: "https://thumbs.dreamstime.com/b/il-robot-d-si-allinea-alla-donna-due-mani-una-posizione-proposta-intelligenza-artificiale-progettazione-aziendale-amicizia-tra-181841302.jpg"
       }
     }
   }).mount('#app')
